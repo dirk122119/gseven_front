00e24858461692eb093604df7797384c4158a8f1
dirk122119 <dirk122119@gmail.com>
2023-05-10 14:36:32 +0800

    add search function and text component

--- app/components/BasicSelect.tsx
+++ app/components/BasicSelect.tsx
@@ -5,6 +5,7 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import BasicTable from './Basictable';
+import TextField from '@mui/material/TextField';
 
 interface Props {
     token: string
@@ -16,14 +17,20 @@ export default function BasicSelect(props: Props) {
     const [catagory, setCatagory] = React.useState("0");
     const [catagories, setCatagories] = React.useState([]);
     const [items,setItems]= React.useState([]);
+    const [message, setMessage] = React.useState('');
+
     const handleChange = (event: SelectChangeEvent) => {
         setCatagory(event.target.value as string);
         let index:number=Number(event.target.value)
         if(index-1>=0){
             getItemsByCatagory(catagories[index-1])
         }
+        else{
+            getAllItems()
+        }
         
     };
+
     const getCatagories= async()=>{
         let myHeaders = new Headers();
 
@@ -74,6 +81,29 @@ export default function BasicSelect(props: Props) {
             .catch(error => console.log('error', error));
     }
 
+    const getItemsBySearch = async(value:string)=>{
+        let myHeaders = new Headers();
+
+        myHeaders.append("Authorization", `Bearer ${props.token}`);
+        let getRequestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+        };
+        await fetch(`https://35.76.111.9/items/search?item=${value}`, getRequestOptions)
+            .then(response => response.json())
+            .then(result => setItems(result))
+            .catch(error => console.log('error', error));
+    }
+
+    const handleKeyPress=(data: any) => {
+        if (data.charCode === 13) {
+            getItemsBySearch(message)
+        }
+      }
+
+    const handleTextfieldChange = (event:any) => {
+        setMessage(event.target.value);
+      };
 
     React.useEffect(() => {
         getCatagories();
@@ -84,7 +114,7 @@ export default function BasicSelect(props: Props) {
     return (
         <>
         <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
+            <FormControl sx={{width:"12vw",paddingRight:"10px"}}>
                 <InputLabel id="demo-simple-select-label">Catagories</InputLabel>
                 <Select
                     labelId="demo-simple-select-label"
@@ -100,11 +130,15 @@ export default function BasicSelect(props: Props) {
 
                 </Select>
             </FormControl>
+
+            <TextField id="outlined-basic" label="Search" variant="outlined" onKeyPress={handleKeyPress} onChange={handleTextfieldChange}/>
         </Box>
-       {  items.length>0 &&
+       {  items.length>0
+       ?
         <Box sx={{paddingTop:"50px"}}>
             <BasicTable items={items}/>
         </Box>
+        :<h1>找不到相關商品</h1>
         }
         </>
     );
